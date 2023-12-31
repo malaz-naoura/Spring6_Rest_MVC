275bf943fdaede4db3805ac9deeaa8637f6d8131
malaz_naoura_dell <malaznaoura@gmail.com>
2023-12-31 20:03:30 +0300

    Create Juice RestMapping verbs

--- src/main/java/mezo/restmvc/spring_6_rest_mvc/controllers/JuiceController.java
+++ src/main/java/mezo/restmvc/spring_6_rest_mvc/controllers/JuiceController.java
@@ -2,11 +2,58 @@ package mezo.restmvc.spring_6_rest_mvc.controllers;
 
 
 import lombok.AllArgsConstructor;
+import mezo.restmvc.spring_6_rest_mvc.model.Juice;
 import mezo.restmvc.spring_6_rest_mvc.service.JuiceService;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+import java.util.UUID;
+
+@RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1/juices")
 public class JuiceController {
     private final JuiceService juiceService;
+
+    @GetMapping
+    List<Juice> listJuice(){
+        return juiceService.listJuices();
+    }
+
+    @GetMapping("/{juideId}")
+    Juice getById(@PathVariable("juideId") UUID juiceId){
+        return juiceService.getJuiceById(juiceId);
+    }
+
+    @PostMapping
+    ResponseEntity saveNewJuice(@RequestBody Juice juice){
+        Juice savedJuice=juiceService.addJuice(juice);
+        HttpHeaders headers=new HttpHeaders();
+        headers.add("Location","api/v1/"+savedJuice.getId());
+        return new ResponseEntity(headers,HttpStatus.CREATED);
+    }
+
+    @PutMapping("{juiceId}")
+    ResponseEntity updateOrCreateJuice(@PathVariable("juiceId") UUID id,@RequestBody Juice juice){
+         juiceService.updateOrCreate(id,juice);
+         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("{juiceId}")
+    ResponseEntity deleteById(@PathVariable("juiceId") UUID id){
+        juiceService.removeById(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @PatchMapping("{juiceId}")
+    ResponseEntity updateJuice(@PathVariable("juiceId") UUID id,@RequestBody Juice juice){
+        juiceService.update(id,juice);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+
+
+    }
+
+
 }
--- src/main/java/mezo/restmvc/spring_6_rest_mvc/service/JuiceService.java
+++ src/main/java/mezo/restmvc/spring_6_rest_mvc/service/JuiceService.java
@@ -2,8 +2,19 @@ package mezo.restmvc.spring_6_rest_mvc.service;
 
 import mezo.restmvc.spring_6_rest_mvc.model.Juice;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JuiceService {
+    List<Juice> listJuices();
+
     Juice getJuiceById(UUID id);
+
+    Juice addJuice(Juice juice);
+
+    Juice updateOrCreate(UUID id, Juice juice);
+
+    void removeById(UUID id);
+
+    void update(UUID id, Juice juice);
 }
