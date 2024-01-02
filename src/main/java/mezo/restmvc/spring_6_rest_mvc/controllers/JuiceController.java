f5a1a88980160e43b2f7afb09d95730bc96fe640
malaz_naoura_dell <malaznaoura@gmail.com>
2024-01-02 21:40:50 +0300

    Add test casese for all methods of JuiceController

--- src/main/java/mezo/restmvc/spring_6_rest_mvc/controllers/JuiceController.java
+++ src/main/java/mezo/restmvc/spring_6_rest_mvc/controllers/JuiceController.java
@@ -15,6 +15,10 @@ import java.util.UUID;
 @AllArgsConstructor
 @RequestMapping("/api/v1/juices")
 public class JuiceController {
+    public static final String JUICE_PATH = "/api/v1/juices";
+    private static final String JUICE_ID_VAR="/{juiceId}";
+    public static final String JUICE_PATH_ID = JUICE_PATH + JUICE_ID_VAR;
+
     private final JuiceService juiceService;
 
     @GetMapping
@@ -22,8 +26,8 @@ public class JuiceController {
         return juiceService.listJuices();
     }
 
-    @GetMapping("/{juideId}")
-    Juice getById(@PathVariable("juideId") UUID juiceId){
+    @GetMapping(JUICE_ID_VAR)
+    Juice getById(@PathVariable("juiceId") UUID juiceId){
         return juiceService.getJuiceById(juiceId);
     }
 
@@ -35,19 +39,19 @@ public class JuiceController {
         return new ResponseEntity(headers,HttpStatus.CREATED);
     }
 
-    @PutMapping("{juiceId}")
+    @PutMapping(JUICE_ID_VAR)
     ResponseEntity updateOrCreateJuice(@PathVariable("juiceId") UUID id,@RequestBody Juice juice){
          juiceService.updateOrCreate(id,juice);
          return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("{juiceId}")
+    @DeleteMapping(JUICE_ID_VAR)
     ResponseEntity deleteById(@PathVariable("juiceId") UUID id){
         juiceService.removeById(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @PatchMapping("{juiceId}")
+    @PatchMapping(JUICE_ID_VAR)
     ResponseEntity updateJuice(@PathVariable("juiceId") UUID id,@RequestBody Juice juice){
         juiceService.update(id,juice);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
---
+++ src/test/java/mezo/restmvc/spring_6_rest_mvc/controllers/JuiceControllerTest.java
@@ -0,0 +1,150 @@
+package mezo.restmvc.spring_6_rest_mvc.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mezo.restmvc.spring_6_rest_mvc.model.Juice;
+import mezo.restmvc.spring_6_rest_mvc.service.JuiceService;
+import mezo.restmvc.spring_6_rest_mvc.service.JuiceServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest(JuiceController.class)
+class JuiceControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    JuiceService juiceService;
+
+    JuiceServiceImpl juiceServiceImpl;
+
+    ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);
+
+    ArgumentCaptor<Juice> juiceArgumentCaptor = ArgumentCaptor.forClass(Juice.class);
+
+
+    @BeforeEach
+    void setUp() {
+        juiceServiceImpl = new JuiceServiceImpl();
+    }
+
+    @Test
+    void listJuice() throws Exception {
+        List<Juice> juiceList = juiceServiceImpl.listJuices();
+
+        given(juiceService.listJuices()).willReturn(juiceList);
+
+        mockMvc.perform(get(JuiceController.JUICE_PATH).accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isOk())
+               .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+               .andExpect(jsonPath("$.length()", is(juiceList.size())));
+    }
+
+    @Test
+    void getById() throws Exception {
+        Juice juice = juiceServiceImpl.listJuices()
+                                      .get(0);
+
+        given(juiceService.getJuiceById(any(UUID.class))).willReturn(juice);
+
+        mockMvc.perform(get(JuiceController.JUICE_PATH_ID, juice.getId()).accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isOk())
+               .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+               .andExpect(jsonPath("$.id", is(juice.getId())))
+               .andExpect(jsonPath("$.juiceName", is(juice.getJuiceName())));
+    }
+
+    @Test
+    void saveNewJuice() throws Exception {
+        Integer lastElement = juiceServiceImpl.listJuices()
+                                              .size() - 1;
+        Juice originalJuice = juiceServiceImpl.listJuices()
+                                              .get(lastElement);
+
+        given(juiceService.addJuice(any(Juice.class))).willReturn(originalJuice);
+
+        Juice newJuice = originalJuice.toBuilder()
+                                      .id(null)
+                                      .version(null)
+                                      .build();
+        newJuice.setId(null);
+        newJuice.setVersion(null);
+
+        mockMvc.perform(post(JuiceController.JUICE_PATH).contentType(MediaType.APPLICATION_JSON)
+                                                        .content(objectMapper.writeValueAsString(newJuice))
+                                                        .accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isCreated())
+               .andExpect(header().exists("Location"));
+    }
+
+    @Test
+    void updateOrCreateJuice() throws Exception {
+
+        Juice juice = juiceServiceImpl.listJuices()
+                                      .get(0);
+
+        mockMvc.perform(put(JuiceController.JUICE_PATH_ID, juice.getId()).contentType(MediaType.APPLICATION_JSON)
+                                                                         .content(objectMapper.writeValueAsString(juice))
+                                                                         .accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isNoContent());
+
+        verify(juiceService).updateOrCreate(any(UUID.class), any(Juice.class));
+    }
+
+    @Test
+    void deleteById() throws Exception {
+
+        Juice juice = juiceServiceImpl.listJuices()
+                                      .get(0);
+
+        mockMvc.perform(delete(JuiceController.JUICE_PATH_ID, juice.getId()).accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isNoContent());
+
+        verify(juiceService).removeById(uuidArgumentCaptor.capture());
+
+        assertThat(juice.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+
+    }
+
+    @Test
+    void updateJuice() throws Exception {
+        Juice juice = juiceServiceImpl.listJuices()
+                                      .get(0);
+
+        juice.setJuiceName("mezoooo");
+
+        mockMvc.perform(patch(JuiceController.JUICE_PATH_ID, juice.getId()).accept(MediaType.APPLICATION_JSON)
+                                                                           .contentType(MediaType.APPLICATION_JSON)
+                                                                           .content(objectMapper.writeValueAsString(juice)))
+               .andExpect(status().isNoContent());
+
+        verify(juiceService).update(uuidArgumentCaptor.capture(), juiceArgumentCaptor.capture());
+
+        assertThat(juice.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+        assertThat(juice.getJuiceName()).isEqualTo(juiceArgumentCaptor.getValue()
+                                                                      .getJuiceName());
+    }
+}
